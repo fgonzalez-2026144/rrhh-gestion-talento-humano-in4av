6862e533c108eacc718a13a4e0e583d2e03bbbaf
Informatica <informatica@alumno.local>
2026-09-22 14:23:21 -0600

    feat: se agrego la conexion

---
+++ src/org/gestiontalenthumano/system/config/Enviroment.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.gestiontalenthumano.system.config;
+
+/**
+ *
+ * @author informatica
+ */
+public class Enviroment {
+    
+    protected static final String USER = "IN4AV";
+    protected static final String PASSWORD = "&mnid4AV";
+    protected static final String DATA_BASE = "rrhh_gestion_talento_humano_in4av";
+    protected static final String LOCATION_SERVICE = "localhost:3306";
+}
